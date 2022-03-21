31b51a74bb13d22c4a3eca89ef052afabb56b084
Mizanur Rahman <mizanbd.eee@gmail.com>
2022-03-22 01:27:07 +0600

    fresh start

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Countries.css';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Hello from REST-countries api</h1>
+        </div>
+    );
+};
+
+export default Countries;
